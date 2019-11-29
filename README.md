eecd439a62601ba2293967c92f7c7c9ae6a985df
MkLHX <mickael.lehoux@gmail.com>
2019-11-29 14:09:00 +0100

    intial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# little_bot
+little bot project with my sons Eliott and Charly
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <TB6612FNG.h>
+
+// # Ultrasonic pin
+#define trigPin A5
+#define echoPin A4
+
+long get_distance()
+{
+	long duration, dist;
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+	duration = pulseIn(echoPin, HIGH);
+	dist = (duration / 2) / 29.1;
+	return dist;
+}
+
+void setup()
+{
+	// put your setup code here, to run once:
+	Serial.begin(115200);
+
+	// # Ultrasonic init
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+	// put your main code here, to run repeatedly:
+	long distance = get_distance();
+
+	Serial.println("distance is: " + String(distance));
+
+	while(distance >= 140){
+		// go forward
+	}
+
+	if(distance < 5){
+		// go backward
+	}
+
+	delay(200);
+}
